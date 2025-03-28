27aea9f65a518ee175be4ac94466f339979bb1b3
Tim Sanders <sanders.timothy.j@gmail.com>
2025-03-28 16:53:53 -0700

    Added memory allocation testing code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import time
+import tracemalloc
 
 from datasets import (generate_structured_datasets,
                       generate_large_random_dataset,
@@ -146,12 +147,50 @@ def time_quick_sort_algorithm(size: int) -> pd.DataFrame:
         })
     return pd.DataFrame(results)
 
+def measure_memory_allocation(size: int) -> pd.DataFrame:
+    results = []
+    datasets = generate_structured_datasets(size)
+    datasets.update(generate_large_random_dataset(1000000))
+    for name, data in datasets.items():
+        n = len(data)
+
+        # Shell Sort (with gap-aware implementation)
+        arr_shell = data.copy()
+        tracemalloc.start()
+        shell_sort(arr_shell)
+        shell_current, shell_peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+
+        # QuickSort
+        arr_quicksort = data.copy()
+        tracemalloc.start()
+        quicksort(arr_quicksort, 0, len(arr_quicksort) - 1)
+        quicksort_current, quicksort_peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+
+        # MergeSort
+        arr_mergesort = data.copy()
+        tracemalloc.start()
+        merge_sort(arr_mergesort, 0, len(arr_mergesort) - 1)
+        merge_current, merge_peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+
+        results.append({
+            "Dataset": name,
+            "Size": n,
+            "Shell Sort Memory": f"{shell_peak / 1024 ** 2:.4f} MB",
+            "QuickSort Memory": f"{quicksort_peak / 1024 ** 2:.4f} MB",
+            "MergeSort Memory": f"{merge_peak / 1024 ** 2:.4f} MB",
+        })
+    return pd.DataFrame(results)
+
 if __name__ == "__main__":
     dataset_size = 10000
     general_results = time_sorting_algorithms(dataset_size)
     shell_sort_results = time_shell_sort_algorithm(dataset_size)
     quicksort_results = time_quick_sort_algorithm(dataset_size)
     merge_sort_results = time_merge_sort_algorithm(dataset_size)
+    memory_allocation_results = measure_memory_allocation(dataset_size)
     print(f"""
 -------------------------
 GENERAL RESULTS
@@ -172,4 +211,9 @@ QUICKSORT RESULTS
 MERGE SORT RESULTS
 -------------------------
 {merge_sort_results}
+
+-------------------------
+MEMORY ALLOCATION RESULTS
+-------------------------
+{memory_allocation_results}
 """)
