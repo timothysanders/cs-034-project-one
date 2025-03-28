d16e21202ad428ab420bc1159c6d1306d0c0b380
Michael Jung <96927527+lolMichaelJung1@users.noreply.github.com>
2025-03-28 11:20:38 -0700

    Update main.py
    Specified the sorting algorithms in their respective measure memory functions

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def measure_quicksort_memory() -> None:
     quicksort(numbers, 0, len(numbers) - 1)
     current, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
-    print(f"Peak memory usage: {peak / 1024**2:.4f} MB")
+    print(f"Peak memory usage for quicksort: {peak / 1024**2:.4f} MB")
 
 
 def measure_merge_sort_memory() -> None:
@@ -46,7 +46,7 @@ def measure_merge_sort_memory() -> None:
     merge_sort(numbers, 0, len(numbers) - 1)
     current, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
-    print(f"Peak memory usage: {peak / 1024**2:.4f} MB")
+    print(f"Peak memory usage for merge sort: {peak / 1024**2:.4f} MB")
 
 
 def measure_shell_sort_memory() -> None:
@@ -67,7 +67,7 @@ def measure_shell_sort_memory() -> None:
     shell_sort(numbers, [88573, 29524, 9841, 3280, 1093, 364, 121, 40, 13, 4])
     current, peak = tracemalloc.get_traced_memory()
     tracemalloc.stop()
-    print(f"Peak memory usage: {peak / 1024**2:.4f} MB")
+    print(f"Peak memory usage for shell sort: {peak / 1024**2:.4f} MB")
 
 if __name__ == "__main__":
     execution_time = timeit.timeit(
